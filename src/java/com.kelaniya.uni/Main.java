5ce96794057d8e25eb838f9766a50a10353150a6
Krishan Shamod <59104070+krishanshamod@users.noreply.github.com>
2021-10-01 13:49:52 +0530

    Modified Main class

--- src/java/com.kelaniya.uni/Main.java
+++ src/java/com.kelaniya.uni/Main.java
@@ -6,9 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
         DataRetrieving dataRetrieving = new DataRetrieving();
-        ArrayList<String[]> monthlyreport=dataRetrieving.displayData();
+        ArrayList<String[]> monthlyReport = dataRetrieving.displayData();
 
+        ReportGeneration reportGeneration = new ReportGeneration(monthlyReport);
+        ArrayList<String[]> finalList = reportGeneration.finalizeRows();
+
+        //print arrayList for testing purposes
+        for (String[] row : finalList) {
+                for (String x : row) {
+                    System.out.printf("%15s", x);
+                }
+                System.out.println("");
+        }
+        
     }
-
-
 }
