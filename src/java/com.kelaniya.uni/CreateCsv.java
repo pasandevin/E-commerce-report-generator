ab18e4f99c0557eef3145e8629ba1c53e9e7bbcf
Krishan Shamod <59104070+krishanshamod@users.noreply.github.com>
2021-10-01 23:38:45 +0530

    Modified CreateCsv class

--- src/java/com.kelaniya.uni/CreateCsv.java
+++ src/java/com.kelaniya.uni/CreateCsv.java
@@ -1,11 +1,9 @@
 package com.kelaniya.uni;
 
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -20,21 +18,7 @@ public class CreateCsv {
     private static final String NEW_LINE_WINDOWS = "\r\n";
 
 
-
-
-    public static void main(String[] args) throws IOException {
-
-
-
-
-        CreateCsv writer = new CreateCsv();
-        writer.writeToCsvFile(createCsvDataSpecial(), new File("C:\\Users\\DELL\\Desktop\\e-commerce-report-generator\\details.csv"));
-         System.out.println("Csv File Created");
-    }
-
     public String convertToCsvFormat(final String[] line) {
-
-
         return convertToCsvFormat(line, DEFAULT_SEPARATOR);
     }
 
@@ -82,7 +66,7 @@ public class CreateCsv {
     }
 
     // a standard FileWriter, CSV is a normal text file
-    private void writeToCsvFile(List<String[]> list, File file) throws IOException {
+    public void writeToCsvFile(List<String[]> list, File file) throws IOException {
 
         List<String> collect = list.stream()
                 .map(this::convertToCsvFormat)
@@ -95,28 +79,6 @@ public class CreateCsv {
                 bw.newLine();
             }
         }
-
-    }
-
-    private static List<String[]> createCsvDataSpecial() {
-
-        String[] header = {"Make", "Model", "Description", "Price"};
-
-        String record[][]={{"Samsung", "", "49\" Dual QHD, QLED, HDR1000", "6199.00"},
-                           {"Dell", "P3421W", "Dell 34, Curved, USB-C Monitor", "2499.00"},
-                           {"Dell", "", "Alienware 38 Curved \"Gaming Monitor\"", "6699.00"},
-                           {"Samsung", "", "Promotion! Special Price\n49\" Dual QHD, QLED, HDR1000", "4999.00"}
-                           };
-
-        List<String[]> list = new ArrayList<>();
-        list.add(header);
-
-        for(String[] x:record){
-            list.add(x);
-
-        }
-
-        return list;
 
     }
 
