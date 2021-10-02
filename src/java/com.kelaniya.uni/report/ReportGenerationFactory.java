23af12b6b0db355314c0e3ee962464d5441f4313
Krishan Shamod <59104070+krishanshamod@users.noreply.github.com>
2021-10-03 00:10:48 +0530

    Added ReportGenerationFactory

---
+++ src/java/com.kelaniya.uni/report/ReportGenerationFactory.java
@@ -0,0 +1,19 @@
+package com.kelaniya.uni.report;
+
+public class ReportGenerationFactory {
+
+    public ReportGeneration getInstance(String arguments){
+
+        ReportGeneration reportGeneration = null;
+
+        //generate the report
+        if(arguments.equals("monthly_sales")){
+            reportGeneration = new MonthlySalesReportGeneration();
+        } else if(arguments.equals("user_signup")) {
+            //not yet implemented
+        }
+
+        return reportGeneration;
+    }
+
+}
