5de3e4dc470bc7a4ab92e721a38bf786e7e49b2d
Krishan Shamod <59104070+krishanshamod@users.noreply.github.com>
2021-10-02 17:25:32 +0530

    Added input validation classes and moved to a separate package

---
+++ src/java/com.kelaniya.uni/input/CommandLineInputs.java
@@ -0,0 +1,62 @@
+package com.kelaniya.uni.input;
+
+public class CommandLineInputs {
+
+    private final String[] args;
+
+    public CommandLineInputs(String[] args) {
+        this.args = args;
+    }
+
+    public String[] validate() {
+
+        if (args.length == 0) {
+            System.out.println("Please provide the arguments. ");
+        }
+
+        String type = args[0];
+        String start_date = args[1];
+        String end_date = args[2];
+        String output_method = args[3];
+        String email_address = args[4];
+
+        String[] validated_input = new String[5];
+
+        //validate the type of report
+        if (!(type.equals("monthly_sales") || type.equals("user_signup"))) {
+            System.out.println("please provide type of report as monthly_sales or user_signup");
+        } else {
+            validated_input[0] = type;
+        }
+
+        //validate start date
+        if (DateValidation.isDateValid(start_date) == false) {
+            System.out.println("Please enter start date in correct format yyyy-mm-dd ");
+        } else {
+            validated_input[1] = start_date;
+        }
+
+        //validate start date
+        if (DateValidation.isDateValid(end_date) == false) {
+            System.out.println("Please enter end date in correct format yyyy-mm-dd ");
+        } else {
+            validated_input[2] = end_date;
+        }
+
+        // validate the output method
+        if (!(output_method.equals("email") || output_method.equals("file"))) {
+            System.out.println("please provide out put method 'email' or 'file' ");
+        } else {
+            validated_input[3] = output_method;
+        }
+
+        //validate email
+        if (EmailValidation.isEmailValid(email_address) == false) {
+            System.out.println("Please enter end date in correct format ex:- joe123@gmail ");
+        } else {
+            validated_input[4] = email_address;
+        }
+
+        return args;
+    }
+}
