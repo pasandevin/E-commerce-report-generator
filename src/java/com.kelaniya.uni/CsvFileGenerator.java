5820f30ed3cc76e165fd345aa6df46b1d7cf6b56
Krishan Shamod <59104070+krishanshamod@users.noreply.github.com>
2021-10-03 01:40:17 +0530

    Modified CsvFileGenerator

---
+++ src/java/com.kelaniya.uni/CsvFileGenerator.java
@@ -0,0 +1,16 @@
+package com.kelaniya.uni;
+
+import com.opencsv.CSVWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class CsvFileGenerator {
+
+    public void generate(List<String[]> csvData) throws IOException {
+        try (CSVWriter writer = new CSVWriter(new FileWriter("monthlySalesReport.csv"))) {
+            writer.writeAll(csvData);
+        }
+    }
+
+}
