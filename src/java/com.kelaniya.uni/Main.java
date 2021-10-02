5e19795eb714909e821bac56dd7e617c8557871c
Krishan Shamod <59104070+krishanshamod@users.noreply.github.com>
2021-10-03 00:11:30 +0530

    Modified Main class

--- src/java/com.kelaniya.uni/Main.java
+++ src/java/com.kelaniya.uni/Main.java
@@ -3,6 +3,7 @@ package com.kelaniya.uni;
 import com.kelaniya.uni.input.CommandLineInputs;
 import com.kelaniya.uni.report.MonthlySalesReportGeneration;
 import com.kelaniya.uni.report.ReportGeneration;
+import com.kelaniya.uni.report.ReportGenerationFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -26,18 +27,9 @@ public class Main {
         DatabaseConnection databaseConnection = new DatabaseConnection();
         Statement statement = databaseConnection.connect();
 
-
-        //create generic report
-        ReportGeneration reportGeneration = null;
-
-        //generate the report
-        if(arguments[0].equals("monthly_sales")){
-            reportGeneration = new MonthlySalesReportGeneration();
-        } else if(arguments[0].equals("user_signup")) {
-            //not yet implemented
-        }
-
         //getting the final report data to array list
+        ReportGenerationFactory reportGenerationFactory = new ReportGenerationFactory();
+        ReportGeneration reportGeneration = reportGenerationFactory.getInstance(arguments[0]);
         ArrayList<String[]> finalReportData = reportGeneration.generate(statement, arguments[1], arguments[2]);
 
         /*
