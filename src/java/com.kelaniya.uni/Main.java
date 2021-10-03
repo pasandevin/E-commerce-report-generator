53092d04e8c1ac68eb4dd203d63aeb3a8ef951d4
Krishan Shamod <59104070+krishanshamod@users.noreply.github.com>
2021-10-03 14:11:37 +0530

    Modified Main class

--- src/java/com.kelaniya.uni/Main.java
+++ src/java/com.kelaniya.uni/Main.java
@@ -1,23 +1,22 @@
 package com.kelaniya.uni;
 
-import com.kelaniya.uni.email.EmailSender;
-import com.kelaniya.uni.email.EmailGenerator;
+import com.kelaniya.uni.export.Exporter;
+import com.kelaniya.uni.export.ExporterFactory;
 import com.kelaniya.uni.input.CommandLineInputs;
+import com.kelaniya.uni.input.Inputs;
 import com.kelaniya.uni.report.ReportGeneration;
 import com.kelaniya.uni.report.ReportGenerationFactory;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 import java.io.IOException;
-import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws IOException, UnirestException {
 
         //validate input arguments
-        CommandLineInputs commandLineInputs = new CommandLineInputs(args);
-        String[] arguments = commandLineInputs.validate();
+        Inputs inputs = new CommandLineInputs(args);
+        String[] arguments = inputs.validate();
 
         String reportType = arguments[0];
         String reportStartDate = arguments[1];
@@ -25,47 +24,20 @@ public class Main {
         String outputMethod = arguments[3];
         String receiverEmail = arguments[4];
 
-
-        //print validated arguments for testing purposes
-//        for(String x: arguments){
-//            System.out.println(x);
-//        }
-
-        //connect database
-        DatabaseConnection databaseConnection = new DatabaseConnection();
-        Statement statement = databaseConnection.connect();
-
         //getting the final report data to array list
         ReportGenerationFactory reportGenerationFactory = new ReportGenerationFactory();
         ReportGeneration reportGeneration = reportGenerationFactory.getInstance(reportType);
-        ArrayList<String[]> finalReportData = reportGeneration.generate(statement, reportStartDate, reportEndDate);
-
-        /*
-        //print report for testing purposes
-        for (String[] row : finalMonthlySalesReportData) {
-            for (String x : row) {
-                System.out.printf("%15s", x);
-            }
-            System.out.println("");
-        }
-        */
+        ArrayList<String[]> finalReportData = reportGeneration.generate(reportStartDate, reportEndDate);
 
         //generate csv file
         CsvFileGenerator csvFileGenerator = new CsvFileGenerator();
         csvFileGenerator.generate(finalReportData);
 
-        //outputs
 
-        if (Objects.equals(outputMethod, "email")) {
-            EmailGenerator emailGenerator = new EmailGenerator(reportType,receiverEmail);
-            String [] emailContents = emailGenerator.getEmailData();
-            String emailSubject = emailContents[0];
-            String emailBody = emailContents[1];
-
-            EmailSender emailsender = new EmailSender(reportType,receiverEmail, emailSubject, emailBody);
-            emailsender.sendMail();
-
-        }
+        //export generated file
+        ExporterFactory exporterFactory = new ExporterFactory();
+        Exporter exporter = exporterFactory.getInstance(outputMethod, reportType, receiverEmail);
+        exporter.export();
 
        
     }
